9ba613affb2c655bd68d2e36cd5c026a720907b2
Chathurga <chathurga@gmail.com>
2014-04-14 18:12:53 +0100

    Added "code golfed" solution to Q1 of preliminary

---
+++ Google Code Jam/magic.hs
@@ -0,0 +1,8 @@
+import Control.Monad
+solve [a, b] = ["Volunteer cheated!", show $ head xs+0, "Bad magician!"] !! i
+  where xs = let g a = (snd a!!((fst a)-1)) in filter (`elem` g b) (g a)
+        i = (\n -> if n > 2 then 2 else n) $ length xs
+parse = replicateM 2 . liftM2 (,) readLn
+      . replicateM 4 $ fmap (map read . words) getLine
+display i = fmap ((("Case #" ++ show i ++ ": ") ++) . solve) parse
+main = fmap (enumFromTo 1) readLn >>= mapM display >>= mapM_ putStrLn
