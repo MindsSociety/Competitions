683450660e4bd2fb1f9a12303d068f3c753484d4
Chathurga <chathurga@gmail.com>
2014-04-14 18:19:22 +0100

    Made line 3 clearer

--- Google Code Jam/magic.hs
+++ Google Code Jam/magic.hs
@@ -1,7 +1,7 @@
 import Control.Monad
 solve [a, b] = ["Volunteer cheated!", show $ head xs+0, "Bad magician!"] !! i
-  where xs = let g a = (snd a!!((fst a)-1)) in filter (`elem` g b) (g a)
-        i = (\n -> if n > 2 then 2 else n) $ length xs
+  where xs = let g (r, b) = (b !! (r-1)) in filter (`elem` g b) (g a)
+        i = (\n -> if n > 2 then 2 else n) $ length xs 
 parse = replicateM 2 . liftM2 (,) readLn
       . replicateM 4 $ fmap (map read . words) getLine
 display i = fmap ((("Case #" ++ show i ++ ": ") ++) . solve) parse
