d8c09c2ad798c93c5c3a2a3d9f58eda040f22488
Chathurga <chathurga@gmail.com>
2014-04-15 16:31:31 +0100

    Remove some brackets

--- Google Code Jam/magic.hs
+++ Google Code Jam/magic.hs
@@ -1,5 +1,5 @@
 (diff, rep) = (\[a,b] -> filter (`elem` b) a, \i -> flip mapM [1..i] . const)
 ans x = ("Volunteer cheated!":head x:repeat "Bad magician!") !! length x
-parse = readLn >>= (\r -> fmap (!!pred r) $ rep 4 (fmap words getLine))
-out i = fmap ((("Case #" ++ show i ++ ": ") ++) . ans . diff) (rep 2 parse)
+parse = readLn >>= (\r -> fmap (!! pred r) . rep 4 $ fmap words getLine)
+out i = fmap ((("Case #" ++ show i ++ ": ") ++) . ans . diff) $ rep 2 parse
 main = fmap (enumFromTo 1) readLn >>= mapM out >>= mapM_ putStrLn
