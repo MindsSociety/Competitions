a43342406c79feba05d4be4e9c4d51c2bca1613f
Chathurga <chathurga@gmail.com>
2014-04-15 15:23:55 +0100

    Index directly into the result list with length xs

--- Google Code Jam/magic.hs
+++ Google Code Jam/magic.hs
@@ -1,7 +1,6 @@
 import Control.Monad
-solve [a, b] = ["Volunteer cheated!", head xs, "Bad magician!"] !! i
-  where xs = let g (r, b) = (b !! (r-1)) in filter (`elem` g b) (g a)
-        i = (\n -> if n > 2 then 2 else n) $ length xs 
+solve [a,b] = ("Volunteer cheated!":head l:repeat "Bad magician!") !! length l
+  where l = let g (r,b) = (b !! (r-1)) in filter (`elem` g b) (g a)
 parse = replicateM 2 . liftM2 (,) readLn . replicateM 4 $ fmap words getLine
 display i = fmap ((("Case #" ++ show i ++ ": ") ++) . solve) parse
 main = fmap (enumFromTo 1) readLn >>= mapM display >>= mapM_ putStrLn
