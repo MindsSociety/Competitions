d3a3e6aba0f6af6dbcc035e78dc5ad29bc257793
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-05-20 20:30:20 +0100

    Create ProblemB_CookieClickerAlpha.java

---
+++ Google Code Jam/ProblemB_CookieClickerAlpha.java
@@ -0,0 +1,32 @@
+package Google_CodeJam_Qualification_2014;
+
+import java.util.Scanner;
+
+public class ProblemB_CookieClickerAlpha {
+	static int MAXFARMS = 1000000;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int t = 1; t <= T; t++) {
+			double c = in.nextDouble();
+			double f = in.nextDouble();
+			double x = in.nextDouble();
+			int curFarms = 0;
+			double ans = 1e9;
+			double time = 0;
+			boolean done = false;
+			while (!done || curFarms < MAXFARMS) {
+				double tans = (x / (2 + f * curFarms)) + time;
+				if (tans <= ans) {
+					ans = tans;
+				} else {
+					done = true;
+				}
+				time += c / (2 + f * curFarms);
+				curFarms++;
+			}
+			System.out.printf("Case #%d: %.7f%n", t, ans + 1e-9);
+		}
+	}
+}
