0726d366da7d78b0fc2d867a693230be08d6f23f
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-05-11 10:25:13 +0100

    Create Missing_Number.hs

---
+++ Programmathon-2015-Maynooth-University/Missing_Number.hs
@@ -0,0 +1,12 @@
+import Data.List
+
+getNumbers :: (Num a, Read a) => IO [a]
+getNumbers = getContents >>= return . map read . lines
+
+-- f :: [a] -> a
+f [] = 0
+f (x:y:xs)
+  | y == succ x = f (y:xs)
+  | otherwise = succ x
+
+main = getLine >> getNumbers >>= print . f . sort
