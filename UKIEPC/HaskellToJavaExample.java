d8b36e52a8cf6448324d6c661ff5b144d5658c4e
Jack Higgins <Jiggins@users.noreply.github.com>
2015-05-11 11:31:07 +0100

    Create HaskellToJavaExample.java

---
+++ UKIEPC/HaskellToJavaExample.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class HaskellToJavaExample {
+  public static void main (String [] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+
+    if (n == 1) {System.out.println("2");}
+    if (n == 2) {System.out.println("4");}
+    if (n == 3) {System.out.println("8");}
+    if (n == 4) {System.out.println("16");}
+    if (n == 5) {System.out.println("32");}
+    if (n == 6) {System.out.println("64");}
+    if (n == 7) {System.out.println("128");}
+    if (n == 8) {System.out.println("256");}
+    if (n == 9) {System.out.println("512");}
+    if (n == 10) {System.out.println("1024");}
+    if (n == 11) {System.out.println("2048");}
+    if (n == 12) {System.out.println("4096");}
+    if (n == 13) {System.out.println("8192");}
+    if (n == 14) {System.out.println("16384");}
+    if (n == 15) {System.out.println("32768");}
+    if (n == 16) {System.out.println("65536");}
+    if (n == 17) {System.out.println("131072");}
+    if (n == 18) {System.out.println("262144");}
+    if (n == 19) {System.out.println("524288");}
+    if (n == 20) {System.out.println("1048576");}
+    if (n == 21) {System.out.println("2097152");}
+    if (n == 22) {System.out.println("4194304");}
+    if (n == 23) {System.out.println("8388608");}
+    if (n == 24) {System.out.println("16777216");}
+    if (n == 25) {System.out.println("33554432");}
+  }
+}
