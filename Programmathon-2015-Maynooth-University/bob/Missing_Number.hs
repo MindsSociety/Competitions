d9dbdcf5da44bc3d958e9b6ea0666ec877d432e0
Jack Higgins <jackhiggins07@gmail.com>
2015-05-13 09:51:26 +0100

    Added type definition for function

--- Programmathon-2015-Maynooth-University/bob/Missing_Number.hs
+++ Programmathon-2015-Maynooth-University/bob/Missing_Number.hs
@@ -3,7 +3,7 @@ import Data.List
 getNumbers :: (Num a, Read a) => IO [a]
 getNumbers = getContents >>= return . map read . lines
 
--- f :: [a] -> a
+f :: (Enum a, Eq a, Num a) => [a] -> a
 f [] = 0
 f (x:y:xs)
   | y == succ x = f (y:xs)
